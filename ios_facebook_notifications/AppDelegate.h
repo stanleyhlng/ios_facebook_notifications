708b5a41800d74eccdde67ab8c917c963897f228
Stanley Ng <stanleyhlng@yahoo.com.hk>
2014-07-08 22:19:58 -0700

    Initial Commit

---
+++ ios_facebook_notifications/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ios_facebook_notifications
+//
+//  Created by Stanley Ng on 7/8/14.
+//  Copyright (c) 2014 Stanley Ng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
