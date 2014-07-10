5fbc8b81c2db382e90bf5c7213dedf66d580c77b
Stanley Ng <stanleyhlng@yahoo.com.hk>
2014-07-10 00:09:08 -0700

    setup feed & more views

--- ios_facebook_notifications/FeedViewController.m
+++ ios_facebook_notifications/FeedViewController.m
@@ -9,7 +9,14 @@
 #import "FeedViewController.h"
 
 @interface FeedViewController ()
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (strong, nonatomic) UIRefreshControl *refreshControl;
 
+- (void) handleLoad;
+- (void) handleRefresh;
+- (void) setupRefreshControl;
+- (void) setupScrolView;
 @end
 
 @implementation FeedViewController
@@ -28,12 +35,53 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    [self setupScrolView];
+    [self setupRefreshControl];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation {
+    CGSize rect = self.scrollView.frame.size;
+    NSLog(@"scrollview: did rotate w%f h%f", rect.width, rect.height);
+}
+
+- (void) handleLoad
+{
+    NSLog(@"handle load");
+    [self.refreshControl endRefreshing];
+}
+
+- (void) handleRefresh
+{
+    NSLog(@"handle refresh");
+    [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(handleLoad) userInfo:nil repeats:NO];
+}
+
+- (void) setupRefreshControl
+{
+    NSLog(@"setup refresh control");
+    
+    // Initialize Refresh Control
+    self.refreshControl = [[UIRefreshControl alloc] init];
+    
+    // Configure Refresh Control
+    [self.refreshControl addTarget:self action:@selector(handleRefresh) forControlEvents:UIControlEventValueChanged];
+    
+    // Configure View Controller
+    [self.scrollView addSubview:self.refreshControl];
+}
+
+- (void) setupScrolView
+{
+    //[self.imageView sizeToFit];
+    
+    //CGRect frame = self.imageView.frame;
+    //[self.scrollView setContentSize:frame.size];
 }
 
 @end
